d92790e51317f8fcc8f6a90f4d79d89a3fedc7a0
AhmedAliHassanElsetouhy <ahmed.ali.hassan.elsetouhy@gmail.com>
2023-05-13 22:03:03 +0300

    Add space value for Data Validation class

--- src/test/java/com/qacart/todo/data/ValidationData.java
+++ src/test/java/com/qacart/todo/data/ValidationData.java
@@ -13,6 +13,5 @@ public class ValidationData {
     public static final String SHORT_STRING_FORMAT = "A";
     public static final String SPECIAL_CHAR = "!#@#@$$!%@^&@*@*(@";
     public static final String SQL_INJECTION_SPECIAL_CHARS = "='+;--??{}";
-    
-
+    public static final String SPACE_Value = "   ";
 }
